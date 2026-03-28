c8e2dbd86c9376b46f72b1c6c0410f15ffa07c0d
Danu <danumertha777@gmail.com>
2026-03-29 01:56:54 +0700

    menambahkan class DataGeneric

--- TP_MODUL5_103022400137/obj/Debug/net10.0/TP_MODUL5_103022400137.AssemblyInfo.cs
+++ TP_MODUL5_103022400137/obj/Debug/net10.0/TP_MODUL5_103022400137.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_MODUL5_103022400137")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50ddb5b72bafd0fa8b7487f0117ea6f9cc77224c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_MODUL5_103022400137")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_MODUL5_103022400137")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
